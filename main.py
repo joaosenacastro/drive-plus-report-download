b501cce2a8aafe700de56facbd3caef2a2a18a14
jsenacas <joacastr1@publicisgroupe.net>
2024-01-17 15:15:17 +0000

    Added the Selenium code to login into the Driveplus website along with some prints to test the directories

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from keboola.component import CommonInterface
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
 import logging
 import csv
+import os
+
 
 if __name__ == "__main__":
     ci = CommonInterface()
@@ -12,9 +16,53 @@ if __name__ == "__main__":
     password = params["#password"]
 
     logging.info(f"Username: {username}")
+    list_of_downloads = []
 
     with open('/data/in/tables/emails.csv') as emails_table:
         emails = csv.DictReader(emails_table)
         for email in emails:
-            print(email['ui'], email['body_html'])
+            list_of_downloads.append(email['download_url'])
+
+    if len(list_of_downloads) == 0:
+        logging.error("No reports to be downloaded. JOB will terminate")
+        exit(1)
+    elif len(list_of_downloads) > 1:
+        logging.info("There is more than 1 report to be downloaded. Only the most recent one will be processed")
+
+    url_to_download = list_of_downloads[-1]
+
+    logging.info(f"The URL of the report to be downloaded is: {url_to_download}")
+
+    logging.info("Starting the selenium process to download the report")
+
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": "/data/out/tables/"}
+    chrome_options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url_to_download)
+
+    username_field = driver.find_element(By.XPATH, "//*[@id=\"mainContainer\"]/div/div/div/div/app-login/div[2]/div[3]/div/form/div[1]/input")
+    username_field.clear()
+    username_field.send_keys(username)
+
+    password_field = driver.find_element(By.XPATH, "//*[@id=\"mainContainer\"]/div/div/div/div/app-login/div[2]/div[3]/div/form/div[2]/input")
+    password_field.clear()
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+
+    print(os.getcwd())
+    os.chdir("/data/out/tables/")
+    print(os.getcwd())
+    print(os.listdir())
+
+
+
+
+
+
+
+
+
+
 
